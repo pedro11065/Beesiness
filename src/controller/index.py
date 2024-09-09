52941c006bf2b16be49be3b19df2e3c0d5fe6f3e
Pedro Quix <83504805+pedro11065@users.noreply.github.com>
2024-09-09 04:44:37 -0300

    voltou pra onde tava

--- src/controller/index.py
+++ src/controller/index.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
-import os
 
-index = Blueprint('index', __name__, template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')), static_folder='static')
+index = Blueprint('index', __name__, template_folder='templates', static_folder='static')
 
 @index.route('/')
 def home():
     return render_template('index.html')
+
+#template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
