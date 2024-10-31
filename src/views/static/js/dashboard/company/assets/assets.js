c85d8de7887629938f4ea94228559eda75a2bd10
Thiago Almeida <tass2904@hotmail.com>
2024-10-31 10:36:09 -0300

    Correções de html e css dos ativos e passivos

--- src/views/static/js/dashboard/company/assets/assets.js
+++ src/views/static/js/dashboard/company/assets/assets.js
@@ -1,84 +1,99 @@
 document.addEventListener('DOMContentLoaded', async function () {
         const main = document.getElementById('main');
-        const loading = document.getElementById('loading'); // Certifique-se de que a variável loading está definida
-        const loader = document.getElementById('loader');
+        const loading = document.getElementById('loading');
 
         loading.style.display = 'flex';
         main.style.display = 'none';
         
-    try {
-        const cnpj = getCnpjFromUrl(); // Obter o CNPJ da URL
-        const response = await fetch(`/dashboard/assets/${cnpj}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error('Erro na resposta da API: ' + response.statusText);
-        }
-
-        const data = await response.json();
-        console.log(data)
-
-        loading.style.display = 'none';
-        loader.style.display = 'none';
-        main.style.display = 'block';
-
-
-        if (data.value && data.value.length > 0) {
+        try {
+            const cnpj = getCnpjFromUrl();
+            const loading = document.getElementById('loading');
+            const loader = document.getElementById('loader');
+            const main = document.getElementById('main');
             const dataContainer = document.getElementById('data-container');
-            dataContainer.innerHTML = ''; // Limpar o conteúdo anterior, se houver
-            
-            data.value.forEach(asset => {
-
-                if(asset.name != '#!@cash@!#'){
-            
-                    const row = document.createElement('tr');
-                
-                    row.innerHTML = `
-                        <td>${asset.asset_id}</td>
-                        <td class="limited-text name">${asset.name}</td>
-                        <td>${asset.event}</td>
-                        <td>${asset.class}</td>
-                        <td>${formatValueToMoney(asset.value)}</td>
-                        <td class="limited-text">${asset.location}</td>
-                        <td>${asset.acquisition_date}</td>
-                        <td>${asset.status}</td>
-                        <td class="limited-text">${asset.description}</td>
-                        <td>${asset.creation_date}</td>
-                        <td>${asset.creation_time}</td>
-                    `;
-
-                    dataContainer.appendChild(row);
-
-                    // Evento de clique para abrir o modal
-                    row.addEventListener('click', function () {
-                        const details = Array.from(row.children).map(child => child.innerText);
-                        console.log(details)
-                    
-                        document.getElementById('uuid').innerText = details[0];
-                        document.getElementById('name').innerText = details[1];
-                        document.getElementById('event').innerText = details[2];
-                        document.getElementById('class').innerText = details[3];
-                        document.getElementById('value').innerText = details[4];
-                        document.getElementById('location').innerText = details[5];
-                        document.getElementById('acquisition_date').innerText = details[6];
-                        document.getElementById('status').innerText = details[7];
-                        document.getElementById('description').innerText = details[8];
-                        document.getElementById('creation').innerText = `${details[9]} - ${details[10]}`;
-                    
-                        document.getElementById('modal').style.display = 'block';
-                    });
+            const tableHead = document.querySelector('.data-table thead');
+        
+            loading.style.display = 'block';
+            loader.style.display = 'block';
+            main.style.display = 'none';
+        
+            const response = await fetch(`/dashboard/assets/${cnpj}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
                 }
             });
-        } else {
-            console.error('Nenhum ativo encontrado!');
-        }
-    } catch (error) {
-        console.error('Erro ao carregar os dados:', error);
-    }
+        
+            if (!response.ok) {
+                throw new Error('Erro na resposta da API: ' + response.statusText);
+            }
+        
+            const data = await response.json();
+            loading.style.display = 'none';
+            loader.style.display = 'none';
+            main.style.display = 'block';
+        
+            // Limpar conteúdo anterior
+            dataContainer.innerHTML = '';
+        
+            if (data.value && data.value.length === 1 && data.value[0].name === '#!@cash@!#') {
+                const errorDiv = document.createElement('div');
+                errorDiv.className = 'error';
+                errorDiv.innerHTML = '<h1>Nenhum dado encontrado.</h1>';
+                main.appendChild(errorDiv);
+            } else if (data.value && data.value.length > 0) {
+                tableHead.classList.remove('hidden');
+        
+                data.value.forEach(asset => {
+                    if (asset.name !== '#!@cash@!#') {
+                        const row = document.createElement('tr');
+                        row.innerHTML = `
+                            <td>${asset.asset_id}</td>
+                            <td class="limited-text name">${asset.name}</td>
+                            <td>${asset.event}</td>
+                            <td>${asset.class}</td>
+                            <td>${formatValueToMoney(asset.value)}</td>
+                            <td class="limited-text">${asset.location}</td>
+                            <td>${asset.acquisition_date}</td>
+                            <td>${asset.status}</td>
+                            <td class="limited-text">${asset.description}</td>
+                            <td>${asset.creation_date}</td>
+                            <td>${asset.creation_time}</td>
+                        `;
+                        dataContainer.appendChild(row);
+        
+                        row.addEventListener('click', function () {
+                            const details = Array.from(row.children).map(child => child.innerText);
+                            console.log(details)
+                        
+                            document.getElementById('uuid').innerText = details[0];
+                            document.getElementById('name').innerText = details[1];
+                            document.getElementById('event').innerText = details[2];
+                            document.getElementById('class').innerText = details[3];
+                            document.getElementById('value').innerText = details[4];
+                            document.getElementById('location').innerText = details[5];
+                            document.getElementById('acquisition_date').innerText = details[6];
+                            document.getElementById('status').innerText = details[7];
+                            document.getElementById('description').innerText = details[8];
+                            document.getElementById('creation').innerText = `${details[9]} - ${details[10]}`;
+                        
+                            document.getElementById('modal').style.display = 'block';
+                        });
+                    }
+                });
+            } else {
+                const errorDiv = document.createElement('div');
+                errorDiv.className = 'error';
+                errorDiv.innerHTML = '<h1>Nenhum dado encontrado.</h1>';
+                main.appendChild(errorDiv);
+            }
+        } catch (error) {
+            console.error('Erro ao carregar os dados:', error);
+            const errorDiv = document.createElement('div');
+            errorDiv.className = 'error';
+            errorDiv.innerHTML = '<h1>Erro ao procurar as informações.</h1>';
+            main.appendChild(errorDiv);
+        }                
 
     // Fechar o modal ao clicar fora dele
     window.addEventListener('click', function (event) {
