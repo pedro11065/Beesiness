7e30182470a413d47657852d1acf10eedd7d7dfd
Thiago Almeida <tass2904@hotmail.com>
2024-11-28 13:26:32 -0300

    Configuração de html, css e javascript para DRE e balanço patrimonial

---
+++ src/controller/dashboard/functions/info_income_statement.py
@@ -0,0 +1,25 @@
+from flask_login import current_user
+from flask import jsonify
+from datetime import datetime
+
+from src.model.database.company.patrimony.historic.search import db_search_historic
+
+def info_income_statement(company_id, cnpj):
+    info = db_search_historic(company_id)
+    print(f'Info do income_statement:\n{info}')
+    
+    if info is False:
+        return jsonify({'message': 'Erro ao pesquisar no banco de dados, tente mais tarde.'}), 500
+
+    if info is None:
+        return jsonify({'message': 'Nenhum dado registrado no banco de dados.'})
+
+    # Busca todos os dados, filtra pelos que estão com 'creation_date' e formata pelo horário correto.
+    for record in info:
+        if 'creation_date' in record:
+            record['creation_date'] = datetime.fromisoformat(record['creation_date'])
+
+    return jsonify({
+        'historic': info
+    })
+
---
+++ src/views/static/js/dashboard/company/reports/income_statement.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    const main = document.getElementById('main');
+    const loading = document.getElementById('loading');
+
+    function getCnpjFromUrl() {
+        const url = window.location.pathname;
+        const parts = url.split('/');
+        return parts[parts.length - 1];
+    }
+
+    const cnpj = getCnpjFromUrl();
+
+    loading.style.display = 'flex';
+    main.style.display = 'none';
+
+    try {
+        const response = await fetch(`/dashboard/income-statement/${cnpj}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Erro na resposta da API: ' + response.statusText);
+        }
+
+        const data = await response.json();
+
+        loading.style.display = 'none';
+        main.style.display = 'flex';
+
+    } catch (error) {
+        const errorDiv = document.createElement('div');
+        errorDiv.innerHTML = `<div class="error"><h1>Erro ao procurar as informações.</h1></div>`;
+        main.appendChild(errorDiv);
+    }
+})
